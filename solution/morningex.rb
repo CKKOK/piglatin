555e9961f35fc3fa86d05a1745d3c39e291f614c
CK <kok.chee.kean@outlook.com>
2018-04-11 10:19:20 +0800

    better solution

--- solution/morningex.rb
+++ solution/morningex.rb
@@ -1,16 +1,10 @@
 def piglatin(word)
     initial_vowels = /\A[aeiou]+/.match(word)
-    initial_consonants = /\A[^aeiouy]+/.match(word)
-    if word[0,2] == "qu"
-        res = word.slice(2, word.length - 2).concat("quay")
-    elsif word[0,3] == "squ"
-        res = word.slice(3, word.length - 3).concat("squay")
-    elsif initial_vowels != nil
+    initial_consonants = /\A((y)|(squ)|(qu)|[^aeiouy]+)/.match(word)
+    if initial_vowels != nil
         res = word + "way"
-    elsif word[0] == 'y'
-        res = word.slice(1, word.length - 1).concat("yay")
     else
-        res = word.sub(/\A[^aeiouy]+/, '').to_s + initial_consonants.to_s + "ay"
+        res = word.sub(/\A((y)|(squ)|(qu)|[^aeiouy]+)/, '').to_s + initial_consonants.to_s + "ay"
     end
     res
 end
